d9f6fd679f68c2e7d3ecf4dd23303429af2d623f
Farid <fareedibrahim0003@gmail.com>
2023-02-27 20:39:26 +0100

    version control test

--- notes.js
+++ notes.js
@@ -5,6 +5,8 @@
 FOR SUBSEQUENT VERSION control- (git init) to initiate git
 - (git add .) to add everything in the project directory
 - (git status) to confirm if the files have been added to the staging area
-- (git commit -m "frst upload") to commit the stages files 
+- (git commit -m "first upload") to commit the stages files 
 - git push -u origin main
 
+[8:33 PM, 2/27/2023] Ace: https://github.com/Fareed0001/calories-calculator-backend
+[8:34 PM, 2/27/2023] Ace: so as to not ruin the frontend and to keep it as a reference file, I created another repository to show  my progress
